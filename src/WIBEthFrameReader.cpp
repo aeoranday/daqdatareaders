2ca0842a8d2d14a93882a0d9ddf2a1370f352599
Alejandro Oranday <alejandro@oran.day>
2024-12-11 11:33:23 +0100

    Use HDF5Reader's Fragment Paths.

--- src/WIBEthFrameReader.cpp
+++ src/WIBEthFrameReader.cpp
@@ -4,12 +4,15 @@ namespace dunedaq {
 namespace daqdatareaders {
 
 void WIBEthFrameReader::filter_fragment_paths(const std::vector<std::string>& fragment_paths) {
-  if (!m_wibeth_paths.empty())
-    m_wibeth_paths.clear();
+  if (!m_fragment_paths.empty())
+    m_fragment_paths.clear();
 
   for (std::string path : fragment_paths) {
     if (path.ends_with("WIBEth"))
-      m_wibeth_paths.push_back(path);
+      m_fragment_paths.push_back(path);
+  }
+}
+
   }
 }
 
