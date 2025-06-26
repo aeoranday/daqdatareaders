a87cda0851dd08315c2b300f54716129512def62
Alejandro Oranday <alejandro@oran.day>
2025-06-26 16:51:18 +0200

    Use Zero-Copy ADC Getting.
    This results in a faster implementation when getting the ADC values from the frames.

--- include/daqdatareaders/WIBEthFrameReader.hpp
+++ include/daqdatareaders/WIBEthFrameReader.hpp
@@ -24,19 +24,23 @@ class WIBEthFrameReader : public HDF5Reader {
 
 
   public:
+    static const int s_num_channels_per_frame = fddetdataformats::WIBEthFrame::s_num_channels;
+    static const int s_num_samples_per_frame = fddetdataformats::WIBEthFrame::s_time_samples_per_frame;
+    static const int s_chunk_size = s_num_channels_per_frame * s_num_samples_per_frame;
+
     WIBEthFrameReader(const std::string& file_name) : HDF5Reader(file_name) {
       filter_fragment_paths(m_h5_file->get_all_fragment_dataset_paths());
     }
 
     void filter_fragment_paths(const std::vector<std::string>& fragment_paths) override;
 
-    std::vector<std::vector<uint16_t>> read_fragment(const std::string& path);
+    std::vector<uint16_t> read_fragment(const std::string& path);
 
     std::tuple<int, int, int> get_fragment_crate_slot_stream(const std::string& path);
 
-    std::vector<std::vector<uint16_t>> read_all_fragments(const std::vector<std::string>& paths);
+    std::vector<uint16_t> read_all_fragments(const std::vector<std::string>& paths);
 
-    std::vector<std::vector<uint16_t>> get_adcs(const fddetdataformats::WIBEthFrame& frame);
+    void fill_adcs(const fddetdataformats::WIBEthFrame& frame, std::span<uint16_t, s_chunk_size> adc_span);
 };
 
 } // namespace daqdatareaders
--- src/WIBEthFrameReader.cpp
+++ src/WIBEthFrameReader.cpp
@@ -28,39 +28,34 @@ void WIBEthFrameReader::filter_fragment_paths(const std::vector<std::string>& fr
   }
 }
 
-std::vector<std::vector<uint16_t>>
-WIBEthFrameReader::get_adcs(const fddetdataformats::WIBEthFrame& frame) {
-  std::vector<std::vector<uint16_t>> adcs;
-  adcs.reserve(64);
-
+void
+WIBEthFrameReader::fill_adcs(const fddetdataformats::WIBEthFrame& frame, std::span<uint16_t, s_chunk_size> adc_span) {
   const fddetdataformats::WIBEthFrame::word_t (*words_ptr)[fddetdataformats::WIBEthFrame::s_bits_per_adc] = frame.adc_words;
+
+  uint16_t* adc_ptr = adc_span.data();
 
   const int register_alignment = fddetdataformats::WIBEthFrame::s_bits_per_adc * 16;
 
-  for (int t = 0; t < 64; t++) {
+  for (int t = 0; t < s_num_samples_per_frame; t++) {
     const fddetdataformats::WIBEthFrame::word_t *time_sample = *(words_ptr + t);
 
     char* cursor = (char*) time_sample;
 
-    std::vector<uint16_t> sample(64, 0);
-    for (int p = 0; p < 4; p++) {
+    for (int p = 0; p < s_num_channels_per_frame / 16; p++) {
       if (p == 3)
         cursor -= 4;
 
-      __m256i regi = _mm256_lddqu_si256((__m256i*)cursor);
+      __m256i regi = _mm256_loadu_si256((__m256i*)cursor);
 
       if (p == 3)
         regi = _mm256_permutevar8x32_epi32(regi, _mm256_setr_epi32(1, 2, 3, 4, 5, 6, 7, 0));
 
       __m256i expanded_frame = expand_frame(regi);
-      _mm256_storeu_si256(reinterpret_cast<__m256i*>(sample.data() + p*16), expanded_frame);
+      _mm256_storeu_si256(reinterpret_cast<__m256i*>(&adc_ptr[t*64 + p*16]), expanded_frame);
 
       cursor += register_alignment / 8;
     }
-    adcs.push_back(std::move(sample));
   }
-
-  return adcs;
 }
 
 std::tuple<int, int, int>
@@ -73,25 +68,36 @@ WIBEthFrameReader::get_fragment_crate_slot_stream(const std::string& path) {
   return std::make_tuple(int(daq_header.crate_id), int(daq_header.slot_id), int(daq_header.stream_id));
 }
 
-std::vector<std::vector<uint16_t>>
+
+std::vector<uint16_t>
 WIBEthFrameReader::read_fragment(const std::string& path) {
   std::vector<fddetdataformats::WIBEthFrame> frames = HDF5Reader::read_fragment<fddetdataformats::WIBEthFrame>(path);
 
-  std::vector<std::vector<uint16_t>> adcs;
-  adcs.reserve(frames.size() * 64);
-  for (auto frame : frames) {
-    std::vector<std::vector<uint16_t>> new_adcs = get_adcs(frame);
-    adcs.insert(adcs.end(), std::make_move_iterator(new_adcs.begin()), std::make_move_iterator(new_adcs.end()));
+  const size_t num_frames = frames.size();
+  const size_t num_samples = s_num_samples_per_frame * num_frames;
+  std::vector<uint16_t> adcs(num_frames * s_chunk_size);
+
+  for (size_t i = 0; i < num_frames; i++) {
+    std::span<uint16_t, s_chunk_size> chunk(adcs.data() + i * s_chunk_size, s_chunk_size);
+    fill_adcs(frames[i], chunk);
   }
 
-  return adcs;
+  // Prefer to use the tranpose of what was read.
+  std::vector<uint16_t> adcs_transpose(num_frames * s_chunk_size);
+  for (size_t i = 0; i < num_samples; i++) {
+    for (size_t j = 0; j < s_num_channels_per_frame; j++) {
+      adcs_transpose[j * num_samples + i] = adcs[i * s_num_channels_per_frame + j];
+    }
+  }
+
+  return adcs_transpose;
 }
 
-std::vector<std::vector<uint16_t>>
+std::vector<uint16_t>
 WIBEthFrameReader::read_all_fragments(const std::vector<std::string>& paths) {
-  std::vector<std::vector<uint16_t>> adcs;
+  std::vector<uint16_t> adcs;
   for (auto path : paths) {
-    std::vector<std::vector<uint16_t>> new_adcs = read_fragment(path);
+    std::vector<uint16_t> new_adcs = read_fragment(path);
     adcs.insert(adcs.end(), std::make_move_iterator(new_adcs.begin()), std::make_move_iterator(new_adcs.end()));
   }
   return adcs;
